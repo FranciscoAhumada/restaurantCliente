e332edc80aa67633c2edcdc505914d30c98b9a3d
Francisco Ahumada cortes <kingsnakef@MacBook-Air-de-Francisco.local>
2021-02-21 22:34:47 -0300

    init fin primer modulo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Routes, Route} from 'react-router';
+
+import firebase, {FirebaseContext} from './firebase'; 
+
+import Ordenes from './componets/paginas/Ordenes';
+import Menu from './componets/paginas/Menu';
+import NuevoPlatillo from './componets/paginas/NuevoPlatillo';
+import SideBar from './componets/ui/SideBar';
+import FireBaseContext from './firebase/context';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FireBaseContext.Provider
+      value = {{
+        firebase
+      }}
+    >
+      <div className='md:flex min-h-screen'>
+        <SideBar/>
+        <div className='md:w-3/5 xl:w-4/5 p-6'>
+          <Routes>
+            <Route path="/ordenes" element={<Ordenes/>}/>
+            <Route path="/menu" element={<Menu/>}/>
+            <Route path="/nuevo-platillo" element={<NuevoPlatillo/>}/>
+          </Routes>
+        </div>
+      </div>
+    </FireBaseContext.Provider>
   );
 }
 
---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA3ckWOlkQKawCVYmwn-7YamOpq-Gwjh1M",
+  authDomain: "restaurant-1b0c2.firebaseapp.com",
+  projectId: "restaurant-1b0c2",
+  storageBucket: "restaurant-1b0c2.appspot.com",
+  messagingSenderId: "116689894782",
+  appId: "1:116689894782:web:76d902a43ec6817ccbc72f",
+  measurementId: "G-SGKV05EL4X"
+};
+
+  export default firebaseConfig;
